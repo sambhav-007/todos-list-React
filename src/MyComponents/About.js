cd0f76ec6005b3c6be31720cafa2cf0cb213de7e
sambhav-007 <sambhav.sehgal.007@gmail.com>
2025-10-19 16:20:52 +0530

    Add description paragraph to About component

--- src/MyComponents/About.js
+++ src/MyComponents/About.js
@@ -4,6 +4,7 @@ const About = () => {
   return (
     <div>
       This is an about component!!!
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatum culpa natus a praesentium, soluta excepturi dolor ad ut molestiae ipsa porro repudiandae aliquam quia eos dicta, aut ea. Alias provident nulla ducimus natus.</p>
     </div>
   )
 }
